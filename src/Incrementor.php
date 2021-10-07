5432949929e0352508f5d0d52778249acd554aa4
Jason Millis <14942820+Jas-n@users.noreply.github.com>
2021-10-07 13:24:50 +0100

    Save as "full" if incremental but no meta data.

--- src/Incrementor.php
+++ src/Incrementor.php
@@ -1,6 +1,7 @@
 <?php
 namespace Formation\Incrementor;
 
+use Illuminate\Support\Facades\Storage;
 use Throwable;
 use ZipArchive;
 
@@ -15,11 +16,13 @@ class Incrementor{
     public function __construct($dir,string $target='./',bool $is_incremental=true,array $skips=array())
     {
         $this->is_laravel=defined('LARAVEL_START');
-        if(!is_dir($target)){
-            if($this->is_laravel){
-                \Illuminate\Support\Facades\Storage::makeDirectory($target);
-            }else{
-                line(true);
+        if($this->is_laravel){
+            if(!Storage::exists($target)){
+                Storage::makeDirectory($target);
+            }
+        }else{
+            if(!is_dir(ROOT.$target)){
+                mkdir(ROOT.$target,0755,true);
             }
         }
         $skips[]=$target;
@@ -28,7 +31,7 @@ class Incrementor{
         }
         $this->dir           =$dir;
         $this->is_incremental=$is_incremental;
-        $this->target        =$target.'/'.date('Y-m-d_H-i-s').($is_incremental?'-incremental':'').'.zip';
+        $this->target        =$target.'/'.date('Y-m-d_H-i-s').($is_incremental && $meta?'-incremental':'').'.zip';
         $this->skips         =$skips;
     }
     public function run()
@@ -58,16 +61,16 @@ class Incrementor{
             }
             $meta=json_encode($meta, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
             if($this->is_laravel){
-                \Illuminate\Support\Facades\Storage::put($meta_file,$meta);
+                Storage::put($meta_file,$meta);
             }else{
-                line(true);
+                file_put_contents(ROOT.$meta_file,$meta);
             }
             if($to_backup){
                 $archive=new ZipArchive();
                 if($this->is_laravel){
-                    $status=$archive->open(\Illuminate\Support\Facades\Storage::path($this->target), ZipArchive::CREATE);
+                    $status=$archive->open(Storage::path($this->target), ZipArchive::CREATE);
                 }else{
-                    line(true);
+                    $status=$archive->open(ROOT.$this->target, ZipArchive::CREATE);
                 }
                 if ($status!==TRUE) {
                     exit("cannot open <$this->target>\n");
@@ -77,7 +80,7 @@ class Incrementor{
                         if($this->is_laravel){
                             $archive->addFile(base_path($file),$file);
                         }else{
-                            line(true);
+                            $archive->addFile($file,str_replace(ROOT,'',$file));
                         }
                     }catch(Throwable $t){
                         \Log::error($t->getMessage());
